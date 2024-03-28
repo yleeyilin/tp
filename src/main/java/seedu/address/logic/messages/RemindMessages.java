5836212447d37606053677c708277b683ca7d7f0
Leong Jia Yi Janna <jannaleong7@gmail.com>
2024-03-28 10:42:43 +0800

    Add remind messages class

---
+++ src/main/java/seedu/address/logic/messages/RemindMessages.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.messages;
+
+/**
+ * Container for user remind command visible messages.
+ */
+public class RemindMessages extends Messages {
+    public static final String MESSAGE_REMIND_PERSON_SUCCESS = "Woof! %1$s contacts found! \uD83D\uDC36";
+}
