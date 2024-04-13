fa023ecf9db9fe8c30dc23cd7f00ba85fc291906
chiageng <chiageng12@gmail.com>
2024-04-13 13:13:32 +0800

    Update code quality

--- src/main/java/seedu/address/model/person/Pin.java
+++ src/main/java/seedu/address/model/person/Pin.java
@@ -2,16 +2,13 @@ package seedu.address.model.person;
 
 /**
  * Represents a Person's Pin in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidNote(String)}
  */
 public class Pin {
 
     private Boolean isPin;
 
     /**
-     * Constructs an {@code Note}.
-     *
-     * @param note A valid note.
+     * Constructs a {@code Pin}.
      */
     public Pin() {
         isPin = false;
